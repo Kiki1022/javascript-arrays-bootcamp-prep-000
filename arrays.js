ac8367011778bdfeaec53c21651e99f29cb315cf
elegant-delimiter-3950 <kinnari1022@yahoo.com>
2020-07-02 00:34:40 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -19,7 +19,7 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element);
+  push(element);
   return array;
 }
 
