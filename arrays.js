e572dcd7f215be01a885be912c3b759477463c70
elegant-delimiter-3950 <kinnari1022@yahoo.com>
2020-07-02 02:21:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -44,8 +44,8 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 
 function removeElementFromEndOfArray(array){
 
-  array.slice(0, array.length-1);
-  return a;
+  array.slice(array.length-1);
+  return array;
   
 }
 
